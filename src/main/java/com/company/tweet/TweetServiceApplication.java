7a35bd90cb87d5f26b7152b21b3f3e2307ace25b
anshupriya24 <95478780+anshupriya24@users.noreply.github.com>
2023-09-06 13:12:55 +0100

    Update TweetServiceApplication.java

--- src/main/java/com/company/tweet/TweetServiceApplication.java
+++ src/main/java/com/company/tweet/TweetServiceApplication.java
@@ -3,9 +3,12 @@ package com.company.tweet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class TweetServiceApplication {
     public static void main(String[] args) {
     	final Logger LOG = LoggerFactory.getLogger(TweetServiceApplication.class);
